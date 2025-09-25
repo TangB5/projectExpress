878a91de88f8216c14b15304ae998b66637ae226
KingTang <ndohyannick78@gmail.com>
2025-09-25 13:39:22 +0100

    mise a jour du cors dans app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './src/docs/swagger.js';
+import swaggerSpec from './src/swagger.js';
 
 // Importez vos routes ici
 import productRoutes from './src/routes/product.routes.js';
@@ -10,23 +12,30 @@ import userRoutes from './src/routes/user.routes.js';
 import authRoutes from './src/routes/auth.routes.js';
 import orderRoutes from './src/routes/order.routes.js';
 
+dotenv.config();
+
 const app = express();
 
-// Middleware
+// Middleware CORS
 const allowedOrigins = [
-  'http://localhost:5000',
-  'https://projectnext-eight.vercel.app'
+    'http://localhost:3001',
+    'https://projectnext-eight.vercel.app',
+    'https://memo-backend-sigma.vercel.app'
 ];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+const corsOptions = {
+    origin: (origin, callback) => {
+        // Autorise les requêtes sans origine (comme les requêtes de Postman)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true, // Très important pour les cookies et les tokens
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -40,7 +49,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/orders', orderRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Backend is running on Vercel!");
+    res.send("Backend is running on Vercel!");
 });
 
 export default app;
